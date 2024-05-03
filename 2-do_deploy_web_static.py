2d04d34e58f2e07d9b5c10276b4534303b6c30de
belal <belalismail252@gmail.com>
2024-05-03 12:03:14 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+a Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy
 """
 
 from fabric.api import put, run, env
@@ -10,7 +10,18 @@ env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """
+    Returns False if the file at the path archive_path doesn’t exist
+    Upload the archive to the /tmp/ directory of the web server
+    Uncompress the archive to the folder,
+    /data/web_static/releases/<archive filename without extension>,
+    on the web server
+    Delete the archive from the web server
+    Delete the symbolic link /data/web_static/current from the web server
+    Create a new the symbolic link /data/web_static/current on the web server,
+    linked to the new version of your code,
+    (/data/web_static/releases/<archive filename without extension>)
+    """
     if exists(archive_path) is False:
         return False
     try:
