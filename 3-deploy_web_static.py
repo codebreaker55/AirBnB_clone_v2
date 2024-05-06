9413a74882ebca337dc058200de1c8ab6b281c94
belal <belalismail252@gmail.com>
2024-05-06 00:43:38 -0700

    local versions

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -19,7 +19,7 @@ def do_pack():
         if isdir("versions") is False:
             local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
+        local("tar -czf {} --exclude=versions web_static".format(file_name))
         return file_name
     except:
         return None
