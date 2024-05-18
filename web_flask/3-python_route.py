1dc22d887513bff0f52217f7c059d26466d31a8d
belal <belalismail252@gmail.com>
2024-05-18 14:41:43 +0300

    web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-start Flask application
+a script that starts a Flask web application
 """
 
 from flask import Flask
@@ -9,27 +9,51 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /: display “Hello HBNB!”
+    using the option strict_slashes=False in your route definition
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /hbnb: display “HBNB"
+    using the option strict_slashes=False in your route definition
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+def c_is_fun(text):
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /c/<text>: display “C ”, followed by the value of the text variable,
+    (replace underscore _ symbols with a space)
+    using the option strict_slashes=False in your route definition
+    """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+def python_is_cool(text='is cool'):
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /python/<text>: display “Python ”, followed by the value of the, 
+    text variable(replace underscore _ symbols with a space),
+    The default value of text is “is cool”
+    using the option strict_slashes=False in your route definition
+    """
     return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-start Flask application
+a script that starts a Flask web application
 """
 
 from flask import Flask, render_template
@@ -9,39 +9,74 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /: display “Hello HBNB!”
+    using the option strict_slashes=False in your route definition
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /hbnb: display “HBNB"
+    using the option strict_slashes=False in your route definition
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+def c_is_fun(text):
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /c/<text>: display “C ”, followed by the value of the text variable,
+    (replace underscore _ symbols with a space)
+    using the option strict_slashes=False in your route definition
+    """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+def python_is_cool(text='is cool'):
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /python/<text>: display “Python ”, followed by the value of the,
+    text variable(replace underscore _ symbols with a space),
+    The default value of text is "is cool"
+    using the option strict_slashes=False in your route definition
+    """
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def imanumber(n):
-    """display “n is a number” only if n is an integer"""
+def strict_number(n):
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /number/<n>: display “n is a number” only if n is an integer
+    using the option strict_slashes=False in your route definition
+    """
     return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
-    """display a HTML page only if n is an integer"""
+def strict_numbers_templates(n):
+    """
+    web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /number_template/<n>: display a HTML page only if n is an integer:
+    H1 tag: “Number: n” inside the tag BODY
+    using the option strict_slashes=False in your route definition
+    """
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
